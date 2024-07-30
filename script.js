6e694be9e836598b2d1eb3cdac13916965cc4ee1
toby959/toto.git <134654437+toby959@users.noreply.github.com>
2024-07-30 11:45:21 -0300

    Agregamos Javascript al proyecto

---
+++ script.js
@@ -0,0 +1,35 @@
+import {barcelona, roma, paris, londres} from './ciudades.js'; 
+
+let enlaces = document.querySelectorAll('a');
+let tituloElemento = document.getElementById('titulo');
+let subTituloElemento = document.getElementById('subtitulo');
+let parrafoElemento =  document.getElementById('parrafo');
+let precioElemento = document.getElementById('precio');
+ 
+enlaces.forEach(function(enlace) {
+    enlace.addEventListener('click', function() {
+        enlaces.forEach(function (enlace) {
+            enlace.classList.remove('active');
+        })
+        
+        this.classList.add('active');
+
+        let contenido = obtenerContenido(this.textContent)
+
+        tituloElemento.innerHTML = contenido.titulo
+        subTituloElemento.innerHTML = contenido.subTitulo
+        parrafoElemento.innerHTML = contenido.parrafo
+        precioElemento.innerHTML = contenido.precio
+    });
+});
+
+function obtenerContenido(enlace) {
+    let contenido = {
+        'Barcelona': barcelona,
+        'Roma': roma, 
+        'París':  paris, 
+        'Londres': londres
+    };
+    return contenido[enlace];
+}
+ 
